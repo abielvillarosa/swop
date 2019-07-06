341c1bd0eb5d7fafc23f747a98ce42d92bb5a5cf
karlptrck <camota.kp@gmail.com>
2019-07-06 18:12:19 -0400

    Updates

---
+++ precache-manifest.0e5af38113227e2a52356cd384879a9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6f25fcb4a15d0fd7601f0526c92866",
+    "url": "/swop-front-end2/index.html"
+  },
+  {
+    "revision": "eba9a1695e9ab244c223",
+    "url": "/swop-front-end2/static/css/main.255c6837.chunk.css"
+  },
+  {
+    "revision": "ba7897182124dc507b10",
+    "url": "/swop-front-end2/static/js/2.ad685d95.chunk.js"
+  },
+  {
+    "revision": "eba9a1695e9ab244c223",
+    "url": "/swop-front-end2/static/js/main.f9b8a276.chunk.js"
+  },
+  {
+    "revision": "6b5f62730da84e96810d",
+    "url": "/swop-front-end2/static/js/runtime~main.25aff668.js"
+  }
+]);
